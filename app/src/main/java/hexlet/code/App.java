c45d296c23361d45e348d91dc910b781368098db
Andrey <rogatin-av@ya.ru>
2024-10-13 19:21:50 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = Integer.parseInt(scanner.next());
+
+        switch (choice) {
+            case 1:
+                Cli.greeting();
+                break;
+            case 2:
+                Even.play();
+                break;
+            default:
+                // for checkstyle
+        }
+
+        scanner.close();
     }
 }
